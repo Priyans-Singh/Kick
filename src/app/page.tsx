e922304cc50f114cf2c56a3a5f7b14817e4f16a8
Priyans-Singh <priyansingh2001@gmail.com>
2024-10-19 02:44:14 +0530

    landing page-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,169 @@
 import Image from "next/image";
+import Link from "next/link";
+import Header from "@/components/header";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="flex min-h-screen bg-stone-200 w-full items-center flex-col bg-muted/40">
+      <Header />
+      <main className="mx-auto flex flex-col items-center gap-4 sm:px-6 sm:py-0 md:gap-8">
+        {/* Hero section */}
+        <div className="flex w-ful flex-col items-center">
+          <div className="text-cente">
+            <h1 className="text-[225px] font-bold">
+              <span className="text-black">DO IT</span>{" "}
+              <span className="text-red-600">RIGHT</span>
+            </h1>
+          </div>
+          <div className="relative mt-5">
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/heroShoe.svg"
+              height={800}
+              width={1350}
+              alt="Nike Air Max shoe on a yellow fabric background"
+              className="rounded-lg"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="absolute -rotate-90 top-40 -left-28 bg-black text-white text-[20px] font-normal px-2 py-3 text-xs rounded-br-lg">
+              Nike product of the year
+            </div>
+            <div className="absolute bottom-10 left-10 text-white">
+              <h2 className="text-[74px] font-semibold">NIKE AIR MAX</h2>
+              <p className="text-[24px] font-normal w-3/4">
+                Nike introducing the new air max for everyone's comfort
+              </p>
+              <Button className="mt-4 bg-red-600 text-white text-[14px] font-normal px-4 py-2 rounded">
+                SHOP NOW
+              </Button>
+            </div>
+            <div className="absolute bottom-10 right-10 flex flex-col space-y-2">
+              <Image
+                src="/Rectangle 2.png"
+                height={160}
+                width={160}
+                alt="Side view of Nike Air Max shoe"
+                className="rounded-lg"
+              />
+              <Image
+                src="/Rectangle 1.png"
+                height={160}
+                width={160}
+                alt="Back view of Nike Air Max shoe"
+                className="rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Shop section */}
+        <div className="p-8 flex flex-col gap-3 items-center">
+          <div className="flex w-full px-4 justify-between items-end">
+            <h1 className="text-6xl w-2/4 font-bold mb-4">
+              DON'T MISS OUT NEW DROPS
+            </h1>
+            <div className="flex justify-end mb-8">
+              <button className="bg-red-500 text-white py-2 px-4 rounded">
+                SHOP NEW DROPS
+              </button>
+            </div>
+          </div>
+          <div className="grid grid-cols-4 gap-4">
+            <div className="max-w-sm items-center flex flex-col gap-3 px-4 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#" className="bg-white p-3 rounded-3xl">
+                <Image
+                  className="bg-white"
+                  height={350}
+                  width={318}
+                  src="/ShopShoe1.png"
+                  alt="1st page"
+                />
+              </a>
+              <a href="#">
+                <h5 className="my-2 text-2xl font-semibold text-wrap text-gray-900 dark:text-white">
+                  ADIDAS 4DFWD X PARLEY RUNNING SHOES
+                </h5>
+              </a>
+              <Button className="inline-flex w-full items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                VIEW PRODUCT
+              </Button>
+            </div>
+            <div className="max-w-sm items-center flex flex-col gap-3 px-4 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#" className="bg-white p-3 rounded-3xl">
+                <Image
+                  className="bg-white"
+                  height={350}
+                  width={318}
+                  src="/ShopShoe2.png"
+                  alt="1st page"
+                />
+              </a>
+              <a href="#">
+                <h5 className="my-2 text-2xl font-semibold text-wrap text-gray-900 dark:text-white">
+                  ADIDAS 4DFWD X PARLEY RUNNING SHOES
+                </h5>
+              </a>
+              <Button className="inline-flex w-full items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                VIEW PRODUCT
+              </Button>
+            </div>
+            <div className="max-w-sm items-center flex flex-col gap-3 px-4 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#" className="bg-white p-3 rounded-3xl">
+                <Image
+                  className="bg-white"
+                  height={350}
+                  width={318}
+                  src="/ShopShoe3.png"
+                  alt="1st page"
+                />
+              </a>
+              <a href="#">
+                <h5 className="my-2 text-2xl font-semibold text-wrap text-gray-900 dark:text-white">
+                  ADIDAS 4DFWD X PARLEY RUNNING SHOES
+                </h5>
+              </a>
+              <Button className="inline-flex w-full items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                VIEW PRODUCT
+              </Button>
+            </div>
+            <div className="max-w-sm items-center flex flex-col gap-3 px-4 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#" className="bg-white p-3 rounded-3xl">
+                <Image
+                  className="bg-white"
+                  height={350}
+                  width={318}
+                  src="/ShopShoe4.png"
+                  alt="1st page"
+                />
+              </a>
+              <a href="#">
+                <h5 className="my-2 text-2xl font-semibold text-wrap text-gray-900 dark:text-white">
+                  ADIDAS 4DFWD X PARLEY RUNNING SHOES
+                </h5>
+              </a>
+              <Button className="inline-flex w-full items-center px-3 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                VIEW PRODUCT
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        {/* Categories section */}
+        <div className="bg-gray-900 w-full min-h-screen flex flex-col items-end">
+          <div className="flex justify-between  w-full items-end px-[28px]">
+            <div className="text-[74px] text-white font-semibold ">
+              CATEGORIES
+            </div>
+            <div className=" flex gap-2 items-center">
+              <Button className="p-5 rounded-md bg-slate-400 text-black ">
+                <i className="fas fa-chevron-left"></i>
+              </Button>
+              <Button className="p-5 rounded-md bg-white ">
+                <i className="fas fa-chevron-right"></i>
+              </Button>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,196 @@
+import * as React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Copy,
+  CreditCard,
+  File,
+  Home,
+  LineChart,
+  ListFilter,
+  MoreVertical,
+  Package,
+  Package2,
+  PanelLeft,
+  Search,
+  Settings,
+  ShoppingCart,
+  Truck,
+  Users2,
+} from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function Header() {
+  return (
+    <div className="flex w-11/12 m-6 flex-col bg-zinc-50 rounded-2xl sm:gap-4 sm:py-4 sm:pl-14">
+      <header className="sticky top-0 z-30 flex h-14 items-center py-2 gap-4 border-b bg-background sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="sm:hidden">
+              <PanelLeft className="h-5 w-5" />
+              <span className="sr-only">Toggle Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="sm:max-w-xs">
+            <nav className="grid gap-6 text-lg font-medium">
+              <Link
+                href="#"
+                className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+              >
+                <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+                <span className="sr-only">KICKS</span>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <Home className="h-5 w-5" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-foreground"
+              >
+                <ShoppingCart className="h-5 w-5" />
+                Orders
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <Package className="h-5 w-5" />
+                Products
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <Users2 className="h-5 w-5" />
+                Customers
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              >
+                <LineChart className="h-5 w-5" />
+                Settings
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className="container flex justify-between items-center">
+          <div className="flex gap-5 justify-start">
+            {/* <DropdownMenu>
+              <DropdownMenuTrigger asChild> */}
+                <p>New Drops🔥</p>
+              {/* </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu> */}
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <p>Men</p>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <p>Women</p>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className="flex justify-center">
+            <Image
+              src="/Logo.svg"
+              width={150}
+              height={36}
+              alt="Avatar"
+              className="overflow-hidden "
+            />
+          </div>
+          <div className="flex gap-4 items-center">
+            <div className="  flex-1 cursor-pointer md:grow-0">
+              <Search className=" h-6 w-6 text-muted-foreground" />
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="overflow-hidden rounded-full"
+                >
+                  <Image
+                    src="/profile.svg"
+                    width={36}
+                    height={36}
+                    alt="Avatar"
+                    className="overflow-hidden rounded-full"
+                  />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <div className="flex cursor-pointer items-center h-8 w-8 justify-center bg-yellow-300 rounded-full">
+              0
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
